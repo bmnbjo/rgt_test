d8d14be83e48239c74c6cfb49cf369bcb29b63f6
bmnbjo <bmnbjo@gmail.com>
2025-02-18 23:36:52 +0900

    port설정

--- bookstore-backend/server.js
+++ bookstore-backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // 데이터베이스 연결 설정 (환경 변수 사용)
 const db = mysql.createConnection({
     host: process.env.DB_HOST,  
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
